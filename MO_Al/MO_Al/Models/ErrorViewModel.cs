bf0a9cc8f9daf00e11cb4a6134b8aa3791502fb0
Ahmed-BS10 <157908930+Ahmed-BS10@users.noreply.github.com>
2024-04-14 07:21:24 +0300

    Delete MO_Al/MO_Al/Models directory

--- MO_Al/MO_Al/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MO_Al.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
